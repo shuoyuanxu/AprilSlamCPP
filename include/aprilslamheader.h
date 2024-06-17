86a8e0c3a796351560d02f97aca8c821fc796e92
shuoyuanxu <shuoyxu@gmail.com>
2024-06-17 11:10:37 +0100

    Landmark rotation updated

--- include/aprilslamheader.h
+++ include/aprilslamheader.h
@@ -18,6 +18,10 @@
 #include <tf2_ros/transform_listener.h>
 #include <gtsam/geometry/Pose2.h>
 #include <tf2/LinearMath/Quaternion.h>
+#include <tf2/LinearMath/Transform.h>
+#include <tf2/LinearMath/Matrix3x3.h>
+#include <tf2/LinearMath/Vector3.h>
+#include <vector>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h> // For TF2 quaternion conversion functions
 #include <gtsam/inference/Symbol.h>
 #include <gtsam/nonlinear/Values.h>
