7f026ded51727e6c185bacc88579023641dc8d6c
shuoyuan <shuoyxu@gmail.com>
2025-04-10 11:30:34 +0100

    ready to deployed

--- bags/remove_tf.py
+++ bags/remove_tf.py
@@ -5,16 +5,16 @@ import rosbag
 def remove_tf_messages(input_bag, output_bag):
     with rosbag.Bag(output_bag, 'w') as outbag:
         for topic, msg, t in rosbag.Bag(input_bag).read_messages():
-            if topic == '/tf':
+            if topic == '/ms/imu/data':
                 # Skip /tf messages
                 continue
             # Otherwise, keep the message
             outbag.write(topic, msg, t)
 
 if __name__ == "__main__":
-    input_bag_path = "loc2_dlo_failed_badly.bag"
-    output_bag_path = "loc2_dlo_failed_badly_filtered.bag"
+    input_bag_path = "medium_DLO.bag"
+    output_bag_path = "medium_DLO_1.bag"
 
     remove_tf_messages(input_bag_path, output_bag_path)
-    print(f"Created {output_bag_path} without the /tf topic.")
+    print(f"Created {output_bag_path} without the /ms/imu/data topic.")
 
